e13c987fd61b5fd06027c0133ef40f6c3f464821
SleepyToDeath <sleepytodeath@gmail.com>
2014-02-08 00:59:42 +0800

    database_2014_02_08_00_59

---
+++ FM_cfg.h
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+#include<map>
+
+namespace cfg
+{
+
+
+
+
+}
+
+typedef id_t long long;
+
+enum data_type{USER,NEWS};
+
+enum page_type{REGISTER,LOGIN,INFO,MAIN,NEWS,SEARCH,RESULT,FOLLOWEE};
+
+enum cmd_type{};
+
+enum ins_type{};
+
+//enum style_type{LEFT,MIDDLE,RIGHT};
